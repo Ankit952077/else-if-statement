78ac87c8d8439e51b19bb3b83ca4f8e202efce2f
Ankit952077 <116968419+Ankit952077@users.noreply.github.com>
2023-01-23 21:33:06 +0530

    Add files via upload

---
+++ z.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+ 
+ int main()
+ {
+     int score;
+     cin>>score;
+
+     if (score>=90)
+     {
+        /* code */
+        cout<<"ankit";
+     }
+      else if (score>=80)
+      {
+        /* code */
+        cout<<"amit";
+      }
+      else if (score>=70)
+      {
+        /* code */
+        cout<<"arun";
+      }
+      else if (score>=60)
+      {
+        /* code */
+        cout<<"A+";
+      }
+      else if (score>=50)
+      {
+        /* code */
+        cout<<"b+";
+      }
+      else
+      cout<<"fail";
+      
+    return 0;
+
+ }
