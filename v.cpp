414566f8f36153dab4d19dc24c294d72edfa4086
Ankit952077 <116968419+Ankit952077@users.noreply.github.com>
2023-01-23 21:56:13 +0530

    Add files via upload

---
+++ v.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+ 
+ int main()
+ {
+    char ch;
+    cin>>ch;
+    switch (ch)
+    {
+    case 'a': /* constant-expression */
+        
+        cout<<"vo";/* code */
+        break;
+    case 'e':
+    
+    cout<<"vo";
+        break;
+
+    case 'i':
+
+    cout<<"vo";
+      break;
+
+      case 'o':
+    cout<<"vo";
+      break;
+
+     case'u':
+
+    cout<<"vo";
+     break;
+    
+    default:
+     cout<<"co";
+
+       break;
+    }
+    return 0;
+
+ }
